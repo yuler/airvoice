3c945944275b5bf9c2e626bdcf542cbfd94e8f6e
yuler <is.yuler@gmail.com>
2026-06-30 15:22:25 +0800

    refactor: simplify desktop/tray.go to hold shared types

--- desktop/tray.go
+++ desktop/tray.go
@@ -3,18 +3,18 @@ package main
 import (
 	"fmt"
 
-	"github.com/wailsapp/wails/v2/pkg/menu"
-	"github.com/wailsapp/wails/v2/pkg/menu/keys"
 	"github.com/wailsapp/wails/v2/pkg/options/mac"
-	"github.com/wailsapp/wails/v2/pkg/runtime"
 )
 
 type TrayManager struct {
 	app *App
+	platformTray
 }
 
 func NewTrayManager(app *App) *TrayManager {
-	return &TrayManager{app: app}
+	t := &TrayManager{app: app}
+	t.initPlatform()
+	return t
 }
 
 func (t *TrayManager) GetMacOptions() *mac.Options {
@@ -24,25 +24,6 @@ func (t *TrayManager) GetMacOptions() *mac.Options {
 			Message: "Speak on your phone, type on your desktop",
 		},
 	}
-}
-
-func (t *TrayManager) GetTrayMenu() *menu.Menu {
-	m := menu.NewMenu()
-
-	m.AddText("Show Window", keys.CmdOrCtrl("0"), func(_ *menu.CallbackData) {
-		runtime.WindowShow(t.app.ctx)
-	})
-	m.AddText("Hide Window", keys.CmdOrCtrl("h"), func(_ *menu.CallbackData) {
-		runtime.WindowHide(t.app.ctx)
-	})
-	m.AddSeparator()
-	m.AddText(t.statusLabel(), nil, nil)
-	m.AddSeparator()
-	m.AddText("Quit", keys.CmdOrCtrl("q"), func(_ *menu.CallbackData) {
-		runtime.Quit(t.app.ctx)
-	})
-
-	return m
 }
 
 func (t *TrayManager) statusLabel() string {
